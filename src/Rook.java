caaf41089d30f28d23af1b56d58d1fc2140dd80c
Satius D <syutkinaai@gmail.com>
2022-04-12 22:19:59 +0300

    Task 5

---
+++ src/Rook.java
@@ -0,0 +1,23 @@
+public class Rook extends ChessPiece{
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (checkOnBoard(toColumn) && checkOnBoard(toLine)) {
+            return (toColumn == column && toLine != line) || (toLine == line && toColumn != column);
+        }
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
+}
